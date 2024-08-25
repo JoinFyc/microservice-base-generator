3ce58e83567bb61d059fea2dbd2a004f941cc4b7
JoinFyc <fyc139050@163.com>
2024-08-25 13:54:44 +0800

    okhttp add http2 support

--- base-code-generator/common/service/integration/src/main/java/com/fei/base/common/service/integration/config/FeignConfiguration.java
+++ base-code-generator/common/service/integration/src/main/java/com/fei/base/common/service/integration/config/FeignConfiguration.java
@@ -1,12 +1,27 @@
 package com.fei.base.common.service.integration.config;
 
-//import com.mengxiang.base.common.rpc.support.annotation.MXEnableCloud;
+import feign.okhttp.OkHttpClient;
+import okhttp3.Protocol;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import okhttp3.OkHttpClient.Builder;
+
+import java.util.Arrays;
 
 /**
  * 配置feign，建议扫包路径调至尽可能小
+ *
  * @author fei
  * @date 2020-09-22 16:16
  **/
-//@MXEnableCloud(basePackages = {"com.mengxiang.x"})
+@Configuration
 public class FeignConfiguration {
+
+    @Bean
+    public OkHttpClient feignClient() {
+        okhttp3.OkHttpClient okHttpClient = new Builder()
+                .protocols(Arrays.asList(Protocol.HTTP_2, Protocol.HTTP_1_1))
+                .build();
+        return new OkHttpClient(okHttpClient);
+    }
 }
